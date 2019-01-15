736e60dc4fd720f2ea10a97572883b8f94b3e7d9
lori <81104174@qq.com>
2019-01-15 21:19:31 +0800

    初始化

---
+++ src/main/java/com/lori/elasticsearchdemo/ElasticsearchDemoApplication.java
@@ -0,0 +1,14 @@
+package com.lori.elasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchDemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/lori/elasticsearchdemo/ElasticsearchDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lori.elasticsearchdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticsearchDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
